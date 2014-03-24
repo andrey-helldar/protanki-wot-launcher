ce1583545ff56cec7f3dfbfccc2e5766f4c05a9d
Helldar <helldar@ai-rus.com>
2014-03-25 01:45:48 +1000

    Добавлен проект для снятия списка процессов, запрещенных к закрытию

---
+++ !Hell Process List/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace _Hell_Process_List
+{
+    partial class fIndex
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bSave = new System.Windows.Forms.Button();
+            this.bClose = new System.Windows.Forms.Button();
+            this.clbProcesses = new System.Windows.Forms.CheckedListBox();
+            this.bwLoad = new System.ComponentModel.BackgroundWorker();
+            this.bwSave = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // bSave
+            // 
+            this.bSave.Location = new System.Drawing.Point(67, 523);
+            this.bSave.Name = "bSave";
+            this.bSave.Size = new System.Drawing.Size(157, 37);
+            this.bSave.TabIndex = 0;
+            this.bSave.Text = "Сохранить данные";
+            this.bSave.UseVisualStyleBackColor = true;
+            this.bSave.Click += new System.EventHandler(this.bSave_Click);
+            // 
+            // bClose
+            // 
+            this.bClose.Location = new System.Drawing.Point(232, 523);
+            this.bClose.Name = "bClose";
+            this.bClose.Size = new System.Drawing.Size(157, 37);
+            this.bClose.TabIndex = 1;
+            this.bClose.Text = "Выход";
+            this.bClose.UseVisualStyleBackColor = true;
+            this.bClose.Click += new System.EventHandler(this.bClose_Click);
+            // 
+            // clbProcesses
+            // 
+            this.clbProcesses.FormattingEnabled = true;
+            this.clbProcesses.Location = new System.Drawing.Point(12, 12);
+            this.clbProcesses.Name = "clbProcesses";
+            this.clbProcesses.Size = new System.Drawing.Size(456, 499);
+            this.clbProcesses.Sorted = true;
+            this.clbProcesses.TabIndex = 2;
+            // 
+            // bwLoad
+            // 
+            this.bwLoad.WorkerReportsProgress = true;
+            this.bwLoad.WorkerSupportsCancellation = true;
+            this.bwLoad.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bwLoad_RunWorkerCompleted);
+            // 
+            // bwSave
+            // 
+            this.bwSave.WorkerReportsProgress = true;
+            this.bwSave.WorkerSupportsCancellation = true;
+            this.bwSave.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bwSave_RunWorkerCompleted);
+            // 
+            // fIndex
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 572);
+            this.Controls.Add(this.clbProcesses);
+            this.Controls.Add(this.bClose);
+            this.Controls.Add(this.bSave);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "fIndex";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "!Hell Process List";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bSave;
+        private System.Windows.Forms.Button bClose;
+        private System.Windows.Forms.CheckedListBox clbProcesses;
+        private System.ComponentModel.BackgroundWorker bwLoad;
+        private System.ComponentModel.BackgroundWorker bwSave;
+    }
+}
+
---
+++ !Hell Process List/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Management;
+using System.Runtime.InteropServices;
+
+namespace _Hell_Process_List
+{
+    public partial class fIndex : Form
+    {
+        public fIndex()
+        {
+            InitializeComponent();
+
+            if (!bwLoad.IsBusy) { bwLoad.RunWorkerAsync(); }
+        }
+
+        private void bClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void bwLoad_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            clbProcesses.Items.Clear();
+
+            string s = "";
+
+            ManagementObjectSearcher searcher1 = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Process");
+
+            foreach (ManagementObject queryObj in searcher1.Get())
+            {
+                clbProcesses.Items.Add(queryObj["Name"].ToString());
+            }
+        }
+
+        private void bSave_Click(object sender, EventArgs e)
+        {
+            if (!bwSave.IsBusy) { bwSave.RunWorkerAsync(); } else { MessageBox.Show("Подождите завершения предыдущей операции"); }
+        }
+
+        private void bwSave_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            string str = "";
+
+            foreach(object item in clbProcesses.CheckedItems){
+                str += item.ToString().Replace(".exe", "") + Environment.NewLine;
+            }
+
+            if (File.Exists("log.txt")) { File.Delete("log.txt"); }
+
+            File.WriteAllText("log.txt", str);
+
+            MessageBox.Show("Данные успешно сохранены в файл log.txt папке с данной программой");
+        }
+    }
+}
