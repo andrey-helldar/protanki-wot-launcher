8150a98294f0bae44b5e92cad50e4eab122cc930
Helldar <helldar@ai-rus.com>
2014-04-17 02:23:33 +1000

    !Hell PRO Tanki Launcher 1.0.10.171 Release

--- !Hell PRO Tanki Launcher/fIndex.cs
+++ !Hell PRO Tanki Launcher/fIndex.cs
@@ -287,17 +287,15 @@ namespace _Hell_PRO_Tanki_Launcher
                 XDocument doc = XDocument.Load(@"http://ai-rus.com/pro/pro.xml");
 
                 remoteModVersion = new Version(doc.Root.Element("version").Value);
-
-                if (doc.Root.Element(!commonTest ? "tanks" : "test") != null)
-                    remoteTanksVersion = new Version(doc.Root.Element(!commonTest ? "tanks" : "test").Value);
-                else
-                    remoteTanksVersion = new Version("0.0.0.0");
-
+                remoteTanksVersion = new Version(POST("http://ai-rus.com/wot/version/", "code=" + Debug.code + "&user=" + Debug.UserID() + "&version=" + tanksVersion.ToString() + "&test=" + (commonTest ? "1" : "0")));
+                
 
                 // Отправляем данные на сайт
                 if (tanksVersion > remoteTanksVersion)
                 {
-                    remoteTanksVersion = new Version(getResponse("http://ai-rus.com/wot/micro/" + tanksVersion.ToString() + "-" + commonTest.ToString()));
+                    //remoteTanksVersion = new Version(getResponse("http://ai-rus.com/wot/micro/" + tanksVersion.ToString() + "-" + commonTest.ToString()));
+                    //string sa = POST("http://ai-rus.com/wot/version/", "code="+Debug.code+"&version=" + tanksVersion.ToString() + "&test=" + (commonTest ? "1" : "0"));
+                    //MessageBox.Show(sa);
                     tanksUpdates = true;
                 }
                 else
@@ -318,6 +316,32 @@ namespace _Hell_PRO_Tanki_Launcher
             {
                 Debug.Save("bwUpdater_DoWork()", ex.Message);
             }
+        }
+
+        private static string POST(string Url, string Data)
+        {
+            System.Net.WebRequest req = System.Net.WebRequest.Create(Url);
+            req.Method = "POST";
+            req.Timeout = 100000;
+            req.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+            byte[] sentData = Encoding.GetEncoding("Utf-8").GetBytes(Data);
+            req.ContentLength = sentData.Length;
+            System.IO.Stream sendStream = req.GetRequestStream();
+            sendStream.Write(sentData, 0, sentData.Length);
+            sendStream.Close();
+            System.Net.WebResponse res = req.GetResponse();
+            System.IO.Stream ReceiveStream = res.GetResponseStream();
+            System.IO.StreamReader sr = new System.IO.StreamReader(ReceiveStream, Encoding.UTF8);
+            Char[] read = new Char[256];
+            int count = sr.Read(read, 0, 256);
+            string Out = String.Empty;
+            while (count > 0)
+            {
+                String str = new String(read, 0, count);
+                Out += str;
+                count = sr.Read(read, 0, 256);
+            }
+            return Out;
         }
 
 
