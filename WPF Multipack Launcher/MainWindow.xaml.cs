7cf6da22f85363d6915c2157462270f8da0db67c
Helldar <helldar@ai-rus.com>
2014-05-19 01:45:41 +1000

    update 6

--- WPF Multipack Launcher/MainWindow.xaml.cs
+++ WPF Multipack Launcher/MainWindow.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -32,6 +34,11 @@ namespace WPF_Multipack_Launcher
 
         private void MainForm_Loaded(object sender, RoutedEventArgs e)
         {
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Multipack Launcher;component/Resources/back_2.jpg")));
         }
     }
 }
