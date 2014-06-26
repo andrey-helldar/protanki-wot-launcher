f853f539dfc32dc0030c91c583bdaae9708b8478
Helldar <helldar@ai-rus.com>
2014-06-26 15:49:51 +1000

    1.0.15.232 - updated bags for 0.9.1 rev.8 + Project: Processes DB to DLL

--- !Hell Processes to DLL/Form1.Designer.cs
+++ !Hell Processes to DLL/Form1.Designer.cs
@@ -28,25 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewGroup listViewGroup7 = new System.Windows.Forms.ListViewGroup("New items", System.Windows.Forms.HorizontalAlignment.Left);
-            System.Windows.Forms.ListViewGroup listViewGroup8 = new System.Windows.Forms.ListViewGroup("Unchecked", System.Windows.Forms.HorizontalAlignment.Left);
-            System.Windows.Forms.ListViewGroup listViewGroup9 = new System.Windows.Forms.ListViewGroup("Checked", System.Windows.Forms.HorizontalAlignment.Left);
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.ListViewGroup listViewGroup13 = new System.Windows.Forms.ListViewGroup("New items", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup14 = new System.Windows.Forms.ListViewGroup("Unchecked", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup15 = new System.Windows.Forms.ListViewGroup("Checked", System.Windows.Forms.HorizontalAlignment.Left);
+            this.bStart = new System.Windows.Forms.Button();
             this.lvProcesses = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lStatus = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // bStart
             // 
-            this.button1.Location = new System.Drawing.Point(12, 517);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(760, 33);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Save";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.bStart.Location = new System.Drawing.Point(129, 517);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(551, 33);
+            this.bStart.TabIndex = 1;
+            this.bStart.Text = "Save";
+            this.bStart.UseVisualStyleBackColor = true;
+            this.bStart.Click += new System.EventHandler(this.button1_Click);
             // 
             // lvProcesses
             // 
@@ -56,16 +60,16 @@
             this.columnHeader2,
             this.columnHeader3});
             this.lvProcesses.FullRowSelect = true;
-            listViewGroup7.Header = "New items";
-            listViewGroup7.Name = "lvgNew";
-            listViewGroup8.Header = "Unchecked";
-            listViewGroup8.Name = "lvgUnchecked";
-            listViewGroup9.Header = "Checked";
-            listViewGroup9.Name = "lvgChecked";
+            listViewGroup13.Header = "New items";
+            listViewGroup13.Name = "lvgNew";
+            listViewGroup14.Header = "Unchecked";
+            listViewGroup14.Name = "lvgUnchecked";
+            listViewGroup15.Header = "Checked";
+            listViewGroup15.Name = "lvgChecked";
             this.lvProcesses.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
-            listViewGroup7,
-            listViewGroup8,
-            listViewGroup9});
+            listViewGroup13,
+            listViewGroup14,
+            listViewGroup15});
             this.lvProcesses.Location = new System.Drawing.Point(12, 12);
             this.lvProcesses.Name = "lvProcesses";
             this.lvProcesses.Size = new System.Drawing.Size(760, 499);
@@ -87,15 +91,40 @@
             this.columnHeader3.Text = "Description";
             this.columnHeader3.Width = 430;
             // 
+            // lStatus
+            // 
+            this.lStatus.Location = new System.Drawing.Point(12, 517);
+            this.lStatus.Name = "lStatus";
+            this.lStatus.Size = new System.Drawing.Size(111, 33);
+            this.lStatus.TabIndex = 3;
+            this.lStatus.Text = "label1";
+            this.lStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(687, 518);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
             // fIndex
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 562);
-            this.Controls.Add(this.lvProcesses);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.lStatus);
+            this.Controls.Add(this.lvProcesses);
+            this.Controls.Add(this.bStart);
             this.Name = "fIndex";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "!Hell Processes to DLL";
             this.Load += new System.EventHandler(this.fIndex_Load);
             this.ResumeLayout(false);
@@ -104,11 +133,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button bStart;
         private System.Windows.Forms.ListView lvProcesses;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Label lStatus;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- !Hell Processes to DLL/Form1.cs
+++ !Hell Processes to DLL/Form1.cs
@@ -19,6 +19,10 @@ namespace _Hell_Processes_to_DLL
         MySqlCommand cmd;
         MySqlDataReader dr;
 
+        List<string> ToDB = new List<string>();
+        string progress = String.Empty;
+
+
         public fIndex()
         {
             InitializeComponent();
@@ -34,27 +38,29 @@ namespace _Hell_Processes_to_DLL
             while (dr.Read())
             {
                 int i = lvProcesses.Items.Add(dr["id"].ToString()).Index;
-                    lvProcesses.Items[i].SubItems.Add(dr["name"].ToString());
-                    lvProcesses.Items[i].SubItems.Add(dr["desc"].ToString());
+                lvProcesses.Items[i].SubItems.Add(dr["name"].ToString());
+                lvProcesses.Items[i].SubItems.Add(dr["desc"].ToString());
 
-                    if (dr["view"].ToString() == "True")
+                if (dr["view"].ToString() == "True")
+                {
+                    if (dr["check"].ToString() == "True")
                     {
-                        if (dr["check"].ToString() == "True")
-                        {
-                            lvProcesses.Items[i].Checked = true;
-                            lvProcesses.Items[i].Group = lvProcesses.Groups["lvgChecked"];
-                            lvProcesses.Items[i].BackColor = Color.LightGreen;
-                        }
-                        else
-                            lvProcesses.Items[i].Group = lvProcesses.Groups["lvgUnchecked"];
+                        lvProcesses.Items[i].Checked = true;
+                        lvProcesses.Items[i].Group = lvProcesses.Groups["lvgChecked"];
+                        lvProcesses.Items[i].BackColor = Color.LightGreen;
                     }
                     else
-                    {
-                        lvProcesses.Items[i].Group = lvProcesses.Groups["lvgNew"];
-                        lvProcesses.Items[i].BackColor = Color.Azure;
-                    }
+                        lvProcesses.Items[i].Group = lvProcesses.Groups["lvgUnchecked"];
+                }
+                else
+                {
+                    lvProcesses.Items[i].Group = lvProcesses.Groups["lvgNew"];
+                    lvProcesses.Items[i].BackColor = Color.Azure;
+                }
             }
             con.Close();
+
+            lStatus.Text = lvProcesses.Items.Count.ToString();
         }
 
         static string getConString()
@@ -69,22 +75,68 @@ namespace _Hell_Processes_to_DLL
 
         private void button1_Click(object sender, EventArgs e)
         {
-            List<string> ToFile = new List<string>();
+            bStart.Enabled = false;
 
-            string toDB = String.Empty;
+            List<string> ToFile = new List<string>();
 
             for (int i = 0; i < lvProcesses.Items.Count; i++)
             {
-                toDB += String.Format("UDPATE `dle_wot_processes` SET `view`='1', `check`='{0}' WHERE `id`='{1}';", lvProcesses.Items[i].Checked?"1":"0", lvProcesses.Items[i].Text);
-                ToFile.Add(String.Format("\"{0}\",", lvProcesses.Items[i].SubItems[1].Text));
+                ToDB.Add(String.Format("UPDATE `dle_wot_processes` SET `view`='1', `check`='{0}' WHERE `id`='{1}';", lvProcesses.Items[i].Checked ? "1" : "0", lvProcesses.Items[i].Text));
+
+                if (lvProcesses.Items[i].Checked)
+                    ToFile.Add(String.Format("\"{0}\",", lvProcesses.Items[i].SubItems[1].Text));
             }
 
+            File.WriteAllLines("db.sql", ToDB);
             File.WriteAllLines("file.txt", ToFile);
 
-            con.Open();
-            cmd = new MySqlCommand(toDB, con);
-            dr = cmd.ExecuteReader();
-            con.Close();
+            try
+            {
+                timer1.Enabled = true;
+                Task.Factory.StartNew(() => UpdateBD());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private async Task UpdateBD()
+        {
+            try
+            {
+                con.Open();
+
+                decimal max = (decimal)ToDB.Count;
+
+                for (int i = 0; i < ToDB.Count; i++)
+                {
+                    progress = Math.Round(((decimal)i / (decimal)max * 100), 2).ToString() + "% (" + i.ToString() + "/" + max.ToString() + ")";
+                    new MySqlCommand(ToDB[i], con).ExecuteNonQuery();
+                }
+                con.Close();
+
+                MessageBox.Show("Database updated sucessfully");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                timer1.Enabled = false;
+                bStart.Enabled = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lStatus.Text = progress;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            button1.Text = timer1.Enabled ? "true" : "false";
         }
     }
 }
