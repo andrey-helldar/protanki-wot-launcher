1bcec06007de2f777025419405cdbb7b575231c0
Helldar <helldar@ai-rus.com>
2014-06-26 10:06:45 +1000

    1.0.15.232 - updated bags for 0.9.1 rev.8 + Project: Processes DB to DLL

---
+++ !Hell Processes to DLL/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _Hell_Processes_to_DLL
+{
+    public partial class fIndex : Form
+    {
+        public fIndex()
+        {
+            InitializeComponent();
+        }
+
+        private void fIndex_Load(object sender, EventArgs e)
+        {
+            this.Text = Application.ProductName + " v" + Application.ProductVersion;
+        }
+    }
+}
---
+++ !Hell Processes to DLL/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _Hell_Processes_to_DLL
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new fIndex());
+        }
+    }
+}
