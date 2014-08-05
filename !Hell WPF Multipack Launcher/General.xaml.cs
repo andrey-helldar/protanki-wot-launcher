fd201d8cc9c8bf79c4096905e708c74f8094726d
Helldar <helldar@ai-rus.com>
2014-08-05 17:39:23 +1000

    2.3.20.266 dev

--- !Hell WPF Multipack Launcher/General.xaml.cs
+++ !Hell WPF Multipack Launcher/General.xaml.cs
@@ -66,41 +66,75 @@ namespace _Hell_WPF_Multipack_Launcher
             {
                 try
                 {
+                    int pos = 0;
+
                     Dispatcher.BeginInvoke(new ThreadStart(delegate
                     {
+                        Grid gridPanel = new Grid();
+                        gridPanel.Width = double.NaN;
+                        gridPanel.Height = double.NaN;
+                        gridPanel.HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
+                        gridPanel.VerticalAlignment = System.Windows.VerticalAlignment.Stretch;
+
                         for (int i = 0; i < YoutubeClass.Count(); i++)
                         {
-                            Thread thr = new Thread(() =>
-                            {
                                 StackPanel panel = new StackPanel();
-                                panel.Width = 100;
-                                panel.Height = 50;
+                                panel.Height = 70;
+                                panel.Width = double.NaN;
+                                panel.Margin = new Thickness(0, pos, 0, 0);
+                                panel.Background = new SolidColorBrush(Colors.Red);
+                                panel.HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
+                                panel.VerticalAlignment = System.Windows.VerticalAlignment.Top;
+                                panel.Name = "Panel" + i.ToString();
 
-                                Grid gridItem = new Grid();
+                                pos += (int)panel.Height + 10;
+
+                                Grid gridPanel0 = new Grid();
+                                gridPanel0.Width = double.NaN;
+                                gridPanel0.Height = double.NaN;
+                                gridPanel0.HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
+                                gridPanel0.VerticalAlignment = System.Windows.VerticalAlignment.Stretch;
+
+                                RowDefinition gridRow1 = new RowDefinition();
+                                gridRow1.Height = new GridLength(20);
+                                gridPanel0.RowDefinitions.Add(gridRow1);
+                                gridPanel0.RowDefinitions.Add(new RowDefinition());
+
+                                ColumnDefinition gridColumn1 = new ColumnDefinition();
+                                gridColumn1.Width = new GridLength(30);
+                                gridPanel0.ColumnDefinitions.Add(gridColumn1);
+                                gridPanel0.ColumnDefinitions.Add(new ColumnDefinition());
 
                                 Label labelDate = new Label();
                                 try { labelDate.Content = YoutubeClass.List[i].Date; }
                                 catch (Exception) { labelDate.Content = "1970-1-1"; }
-                                gridItem.Children.Add(labelDate);
+                                Grid.SetRow(labelDate, 0);
+                                gridPanel0.Children.Add(labelDate);
 
                                 Button buttonClose = new Button();
-                                buttonClose.Content = "X";
-                                gridItem.Children.Add(buttonClose);
+                                buttonClose.Content = "X :: id: " + i.ToString();
+                                Grid.SetRow(buttonClose, 0);
+                                gridPanel0.Children.Add(buttonClose);
 
                                 Label labelTitle = new Label();
                                 try { labelTitle.Content = YoutubeClass.List[i].Title; }
                                 catch (Exception) { labelTitle.Content = "Error loading (id" + i.ToString() + ")"; }
-                                gridItem.Children.Add(labelTitle);
+                                gridPanel0.Children.Add(labelTitle);
 
-                                panel.Children.Add(gridItem);
-                                Dispatcher.BeginInvoke(new ThreadStart(delegate { svVideo.Content = panel; }));
-                            });
-                            thr.SetApartmentState(ApartmentState.STA);
-                            thr.Start();
-                        }
+                                /*try { this.Dispatcher.BeginInvoke(new ThreadStart(delegate { svVideo.Content = panel; })); }
+                                catch (Exception ex0) { Task.Factory.StartNew(() => Debug.Save("General.xaml", "Page_Loaded()", "Apply video to form", ex0.Message, ex0.StackTrace)); }*/
+
+                                panel.Children.Add(gridPanel0);
+
+                                try { gridPanel.Children.Add(panel); }
+                                catch (Exception ex0) { Task.Factory.StartNew(() => Debug.Save("General.xaml", "Page_Loaded()", "Apply video to form", ex0.Message, ex0.StackTrace)); }
+                            }
+
+                        svVideo.Content = gridPanel;
                     }));
+
                 }
-                catch (Exception ex) { Task.Factory.StartNew(() => Debug.Save("General.xaml", "Page_Loaded()", "Apply video to form", ex.Message)); }
+                catch (Exception ex) { Task.Factory.StartNew(() => Debug.Save("General.xaml", "Page_Loaded()", "Apply video to form", ex.Message, ex.StackTrace)); }
             });
         }
 
@@ -120,21 +154,29 @@ namespace _Hell_WPF_Multipack_Launcher
 
                     for (int i = 0; i < 2; i++) // Если цикл прерван случайно, то выжидаем еще 5 секунд перед повторным запуском
                     {
-                        while (System.Diagnostics.Process.GetProcessesByName("WorldOfTanks").Length > 0 ||
-                            System.Diagnostics.Process.GetProcessesByName("WoTLauncher").Length > 0)
-                            Thread.Sleep(5000);
+                        try
+                        {
+                            while (System.Diagnostics.Process.GetProcessesByName("WorldOfTanks").Length > 0 ||
+                                System.Diagnostics.Process.GetProcessesByName("WoTLauncher").Length > 0)
+                                Thread.Sleep(5000);
 
-                        Thread.Sleep(5000);
+                            Thread.Sleep(5000);
+                        }
+                        catch (Exception) { }
                     }
 
-                    NotifyLink = el.Link;
+                    try
+                    {
+                        NotifyLink = el.Link;
 
-                    Task.Factory.StartNew(() => ShowNotify("Смотреть видео"));
+                        Task.Factory.StartNew(() => ShowNotify("Смотреть видео"));
 
-                    if (XmlGeneral.Root.Element("youtube") != null)
-                        XmlGeneral.Root.Element("youtube").Add(new XElement("video", el.ID));
-                    else
-                        XmlGeneral.Root.Add(new XElement("youtube", new XElement("video", el.ID)));
+                        if (XmlGeneral.Root.Element("youtube") != null)
+                            XmlGeneral.Root.Element("youtube").Add(new XElement("video", el.ID));
+                        else
+                            XmlGeneral.Root.Add(new XElement("youtube", new XElement("video", el.ID)));
+                    }
+                    catch (Exception) { }
                 }
             }
             catch (Exception ex) { Task.Factory.StartNew(() => Debug.Save("General.xaml", "VideoNotify()", ex.Message)); }
