736ff6d63ae2790e474a0311a7b67ea89f13fa8f
Helldar <helldar@ai-rus.com>
2014-09-11 14:59:12 +1000

    2.3.21.278
    	Добавил страницу выбора языка интерфейса

--- !Hell WPF Multipack Launcher/MainWindow.xaml.cs
+++ !Hell WPF Multipack Launcher/MainWindow.xaml.cs
@@ -200,5 +200,10 @@ namespace _Hell_WPF_Multipack_Launcher
             try { MainFrame.NavigationService.Navigate(new Uri("General.xaml", UriKind.Relative)); }
             catch (Exception ex) { Task.Factory.StartNew(() => Debug.Save("MainWindow", "General form loading", ex.Message)); }
         }
+
+        private void bChangeLang_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
