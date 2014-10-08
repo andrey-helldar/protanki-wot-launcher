e8094ac1e22e93b7828480c365148dd8d65d8892
Helldar <helldar@ai-rus.com>
2014-10-08 13:57:02 +1000

    Интерфейс: Feedback

--- !Hell WPF Multipack Launcher/Properties/AssemblyInfo.cs
+++ !Hell WPF Multipack Launcher/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.3.24.621")]
-[assembly: AssemblyFileVersion("2.3.24.621")]
+[assembly: AssemblyVersion("2.3.24.624")]
+[assembly: AssemblyFileVersion("2.3.24.624")]
 [assembly: GuidAttribute("B3F13C67-4CBB-4AA3-B942-F8A285ED1CBF")]
