dd5721475419817d8865c8384dbdc7d4f7ebc7de
Helldar <helldar@ai-rus.com>
2014-10-09 11:45:40 +1000

    Интерфейс: General :: rev.5 ListBox :: Buttons Play & Close ЖЖ Mechanic

--- !Hell PRO Tanki Launcher/Properties/AssemblyInfo.cs
+++ !Hell PRO Tanki Launcher/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.25.642")]
-[assembly: AssemblyFileVersion("1.1.25.642")]
+[assembly: AssemblyVersion("1.1.25.644")]
+[assembly: AssemblyFileVersion("1.1.25.644")]
