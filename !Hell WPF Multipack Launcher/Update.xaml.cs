cf629e880687400dec4621326a868b66ee2e694b
Helldar <helldar@ai-rus.com>
2014-11-27 00:45:27 +0800

    Вроде как готово окно уведомлений

--- !Hell WPF Multipack Launcher/Update.xaml.cs
+++ !Hell WPF Multipack Launcher/Update.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -94,11 +95,60 @@ namespace _Hell_WPF_Multipack_Launcher
             catch (Exception) { }
         }
 
-        private string ParseChangelog(string log) {
-            try {
+        private string ParseChangelog(string log)
+        {
+            try
+            {
                 if (log.Length > 0)
                 {
+                    /*
+                     * В этой версии произошли следующие изменения:
+                     * <font size='12' color='808080'>
+                     *  <ul>
+                     *      <li>Обновлен мод Received Damage Announcer от PROТанки, добавлена функция автоинформирования команды о том, что артиллерия отстрелялась... по вам :( ;</li>
+                     *      <li>Обновлен мод анонсов о стримах с розыгрышами игрового золота, мод стал лучше и теперь можно выбрать режим \"Не беспокоить\", в котором мод не будет напоминать о стриме;</li>
+                     *      <li>Обновлена конфигурация послебоевых сообщение от Armagomen;</li>
+                     *      <li>Обновлена конфигурация сессионной статистики от Armagomen;</li>
+                     *      <li>Обновлена конфигурация послебоевых сообщение от Meddio;</li>
+                     *      <li>Обновлена конфигурация сессионной статистики от Meddio.</li>
+                     *      <li>__________________</li>
+                     *      <li>Буду благодарен, если Вы еще раз посмотрите рекламный ролик в начале видео.</li>
+                     *  </ul>
+                     * </font>__________________________________________<br>
+                     * <font color='#E00000'>Новая версия доступна по ссылке в описании к видео.</font>
+                     */
 
+                    /*
+                     * Удаляем все после данного тега, включая его сам
+                     * Тем самым сокращаем время работы регулярных выражений
+                     */
+                    log = log.Remove(log.IndexOf("</font>"));
+
+                    // Доп обработки строк
+                    log = log.Replace("<ul>", Environment.NewLine + Environment.NewLine);
+                    log = log.Replace("</ul>", "");
+
+                    // Удаляем все теги <font>
+                    Regex regex = new Regex(@"<font(.*)>");
+                    Match match = regex.Match(log);
+                    while (match.Success)
+                    {
+                        try { log = log.Replace(match.Value, ""); }
+                        catch (Exception) { }
+                        match = match.NextMatch();
+                    }
+
+                    /*
+                     *  Обрабатываем элементы списка
+                     */
+                    Regex regexLI = new Regex(@"<li>(.*)</li>");
+                    Match matchLI = regexLI.Match(log);
+                    while (matchLI.Success)
+                    {
+                        try { log = log.Replace(matchLI.Value, " * " + matchLI.Value); }
+                        catch (Exception) { }
+                        matchLI = matchLI.NextMatch();
+                    }
                 }
             }
             catch (Exception ex) { Task.Factory.StartNew(() => Debug.Save("Update.xaml", "ParseChangelog()", ex.Message, ex.StackTrace)); }
