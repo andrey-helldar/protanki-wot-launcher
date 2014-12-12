afae9742d0e19bee425888d6f1163c3e64bdec30
Helldar <helldar@ai-rus.com>
2014-12-12 22:14:38 +0800

    RELEASE 2.5.37.859

--- !Hell WPF Multipack Launcher/Properties/AssemblyInfo.cs
+++ !Hell WPF Multipack Launcher/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.5.36.858")]
-[assembly: AssemblyFileVersion("2.5.36.858")]
+[assembly: AssemblyVersion("2.5.37.859")]
+[assembly: AssemblyFileVersion("2.5.37.859")]
 [assembly: GuidAttribute("B3F13C67-4CBB-4AA3-B942-F8A285ED1CBF")]
