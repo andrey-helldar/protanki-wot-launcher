58b4f6f242bd659b9fb42397d96f9a1fdd62c2e3
Helldar <helldar@ai-rus.com>
2015-01-07 00:58:54 +0800

    RELEASE 2.6.40.877

--- !Hell PRO Tanki Launcher/Properties/AssemblyInfo.cs
+++ !Hell PRO Tanki Launcher/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.41.876")]
-[assembly: AssemblyFileVersion("1.1.41.876")]
+[assembly: AssemblyVersion("1.1.42.877")]
+[assembly: AssemblyFileVersion("1.1.42.877")]
--- !Hell WPF Multipack Launcher/Properties/AssemblyInfo.cs
+++ !Hell WPF Multipack Launcher/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.6.39.876")]
-[assembly: AssemblyFileVersion("2.6.39.876")]
+[assembly: AssemblyVersion("2.6.40.877")]
+[assembly: AssemblyFileVersion("2.6.40.877")]
 [assembly: GuidAttribute("B3F13C67-4CBB-4AA3-B942-F8A285ED1CBF")]
