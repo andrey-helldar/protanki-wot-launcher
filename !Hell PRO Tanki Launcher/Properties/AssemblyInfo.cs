a9e923e8a5605a8941f85692bcba6ac9b46a6303
Helldar <helldar@ai-rus.com>
2015-02-17 23:56:05 +0800

    Не отправляет тикеты

--- !Hell PRO Tanki Launcher/Properties/AssemblyInfo.cs
+++ !Hell PRO Tanki Launcher/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.44.903")]
-[assembly: AssemblyFileVersion("1.1.44.903")]
+[assembly: AssemblyVersion("1.1.45.905")]
+[assembly: AssemblyFileVersion("1.1.45.905")]
